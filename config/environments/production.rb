7db69c3ee41e5f1f3f615233777efaa5396aada2
ボイビ <>
2021-04-10 22:09:01 +0900

    Update gemfile.lock for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 require "active_support/core_ext/integer/time"
-
+config.assets.initialize_on_precompile = false
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
